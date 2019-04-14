aa79693a3944085c234c106b8446fae024f37aaf
Bonggt <kepy1106@gmail.com>
2019-04-14 17:47:50 +0900

    canvas Context

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+// default ctx
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
+
+function startPainting(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  painting = true;
+  ctx.lineTo(x, y);
+  ctx.stroke();
+}
 
 function stopPainting() {
   painting = false;
@@ -9,22 +25,20 @@ function stopPainting() {
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
-  console.log(painting);
-}
-
-function onMouseDown(event) {
-  painting = true;
-  console.log(painting);
-}
-
-function onMouseUp(event) {
-  stopPainting();
-  console.log(painting);
+  if (!painting) {
+    // console.log(`Creating Path In`, x, y);
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    // console.log(`Creating Line In`, x, y);
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
